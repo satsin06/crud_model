8918cabf2f6ac3a5246850d7ea8a00fca8c5bea4
Satyam Sinha <satyamsinha9404@gmail.com>
2021-12-09 19:52:12 +0530

    firebase setup

--- lib/add_list.dart
+++ lib/add_list.dart
@@ -1,12 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddList extends StatelessWidget {
-  const AddList({ Key? key }) : super(key: key);
+  TextEditingController names = TextEditingController();
+  CollectionReference ref = FirebaseFirestore.instance.collection('names');
+  AddList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Names'), centerTitle: true, automaticallyImplyLeading: true,),
+      appBar: AppBar(
+        title: Text('Add Names'),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+      ),
+      body: TextField(
+        autocorrect: true,
+        controller: names,
+        textCapitalization: TextCapitalization.words,
+        decoration: InputDecoration(
+          hintText: "Add Name",
+          hintStyle: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.add({'names': names.text}).whenComplete(
+              () => Navigator.pop(context));
+        },
+        child: Icon(Icons.save),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Crud Model',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
